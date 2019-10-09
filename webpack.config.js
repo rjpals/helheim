18e936361b3aaef7d15437e92e95aad793b48022
Ryan Pals <ryan@hobu.co>
2019-10-09 13:14:46 -0500

    Add webpack CSS bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,51 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    module: {
+        rules: [],
+    },
+};
+
 const path = require('path')
 module.exports = {
     mode: 'production',
     entry: path.join(__dirname, 'index.js'),
-    output: { filename: 'bundle.js' },
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        },
-        {
-          test: /\.css$/,
-          use: {
-              loader: 'css-loader'
-          }
-        }
-      ]
+        rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+        ]
     }
 }
-
