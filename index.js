dba7db3a0babfbb629040bb7c3ff34c7b8cfc98e
Ryan Pals <ryan@hobu.co>
2019-10-11 13:38:00 -0500

    Split out components

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 'use strict';
 import React from 'react'
 import ReactDom from 'react-dom'
-import ReactSelect from 'react-select'
 import Sidebar from './sidebar'
+import Setting from './setting'
+
+const sidebar = <Sidebar>
+    <h1 style={{textAlign: "center"}}> Helheim </h1>
+    <Setting
+        title="Scan speed"
+        desc="Speed at which renderer advances to the next scan"
+        min={0.2}
+        max={10}
+        step={0.1}
+        defaultValue={1}
+        onChange= {
+            (val) => window.movie.speed = 1000 * val
+        }
+    />
+    <Setting
+        title="Point Budget"
+        desc="Total number of points displayed"
+        min={1e4}
+        max={1e9}
+        defaultValue={1e4}
+        onChange= {(val) => window.viewer.setPointBudget(val)}
+    />
+    <Setting
+        title="Look Ahead"
+        desc="Number of scans to preload"
+        onChange= {(val) => window.movie.preload = 1 + val}
+    />
+    <Setting/>
+</Sidebar>
 
 const domContainer = document.querySelector('#react-container');
-ReactDom.render(<Sidebar/>,  domContainer);
+ReactDom.render(sidebar,  domContainer);
 
--- sidebar.js
+++ sidebar.js
@@ -2,54 +2,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import ReactSelect from 'react-select'
-import 'rc-slider/assets/index.css'
-import Slider from 'rc-slider'
-const { Handle } = Slider
-
-class Setting extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {displayVal: props.defaultValue}
-    }
-
-    componentDidMount() {
-        //this.props.onChange(this.props.defaultValue)
-        console.log('yeet')
-    }
-
-    render() {
-        const display = this.state.displayVal.toLocaleString()
-        return <div> 
-            <h1> {this.props.title} </h1>
-            <p> {this.props.desc} </p>
-            <div style={{textAlign: 'center'}}>
-                { display }
-                <Slider
-                    min={this.props.min}
-                    max={this.props.max}
-                    defaultValue={this.props.defaultValue}
-                    step={this.props.step}
-                    onChange={ (val)=> {
-                        this.setState({displayVal: val})
-                        this.props.onChange(val)
-                        }
-                    }
-                    handle={ props => <Handle {...props}/> }
-                />
-            </div>
-
-        </div>
-    }
-}
-
-Setting.defaultProps = {
-    min: 0,
-    max: 10,
-    defaultValue: 5,
-    onChange: console.log,
-    title: "Insert title",
-    desc: "Insert description",
-}
 
 export default class Sidebar extends React.Component {
     constructor(props) {
@@ -101,32 +53,7 @@ export default class Sidebar extends React.Component {
                 }}
             >
                 {openButton}
-                <h1 style={{textAlign: "center"}}> Helheim </h1>
-                <Setting
-                    title="Scan speed"
-                    desc="Speed at which renderer advances to the next scan"
-                    min={0.2}
-                    max={10}
-                    step={0.1}
-                    defaultValue={1}
-                    onChange= {
-                        (val) => window.movie.speed = 1000 * val
-                    }
-                />
-                <Setting
-                    title="Point Budget"
-                    desc="Total number of points displayed"
-                    min={1e4}
-                    max={1e9}
-                    defaultValue={1e6}
-                    onChange= {(val) => window.viewer.setPointBudget(val)}
-                />
-                <Setting
-                    title="Look Ahead"
-                    desc="Number of scans to preload"
-                    onChange= {(val) => window.movie.preload = 1 + val}
-                />
-                <Setting/>
+                {this.props.children}
                 {pauseButton}
             </div>
         } else {
