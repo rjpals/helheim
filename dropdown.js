b05380027e4fcedb23d9ccffee505e38fe1b6fe1
Ryan Pals <ryan@hobu.co>
2019-10-12 12:28:16 -0500

    Split out dropdown

---
+++ dropdown.js
@@ -0,0 +1,45 @@
+'use strict';
+import React from 'react'
+import ReactDom from 'react-dom'
+import Sidebar from './sidebar'
+import Setting from './setting'
+
+export default class Dropdown extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { open: true }
+    }
+
+    render() {
+        const openButton = <button
+            onClick={ () => this.setState({ open: !this.state.open})}
+            style = {{
+                width: "100%"
+            }}
+            >
+               <h2> {this.props.title} </h2>
+            </button>
+
+        if(this.state.open) {
+            return <div>
+                <br/>
+                {openButton}
+                <br/>
+                <div style={{
+                    borderWidth: "1px",
+                    borderStyle: "solid",
+                    borderColor: "grey",
+                }} >
+                    {this.props.children}
+                </div>
+            </div>
+        } else {
+            return <div> <br/> {openButton} <br/> </div>
+        }
+    }
+}
+
+Dropdown.defaultProps = {
+    title: "Placeholder title",
+}
+
--- index.js
+++ index.js
@@ -3,45 +3,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Sidebar from './sidebar'
 import Setting from './setting'
-
-class Dropdown extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { open: true }
-    }
-
-    render() {
-        const openButton = <button
-            onClick={ () => this.setState({ open: !this.state.open})}
-            style = {{
-                width: "100%"
-            }}
-            >
-               <h2> {this.props.title} </h2>
-            </button>
-
-        if(this.state.open) {
-            return <div>
-                <br/>
-                {openButton}
-                <br/>
-                <div style={{
-                    borderWidth: "1px",
-                    borderStyle: "solid",
-                    borderColor: "grey",
-                }} >
-                    {this.props.children}
-                </div>
-            </div>
-        } else {
-            return <div> <br/> {openButton} <br/> </div>
-        }
-    }
-}
-
-Dropdown.defaultProps = {
-    title: "Placeholder title",
-}
+import Dropdown from './dropdown'
 
 const sidebar = <Sidebar>
     <h1 style={{textAlign: "center"}}> Helheim </h1>
