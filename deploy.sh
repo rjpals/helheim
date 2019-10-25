c36287473fbb4d826b143fb8893bda0ba1692855
Ryan Pals <ryan@hobu.co>
2019-10-25 11:50:48 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,21 @@ echo Beginning deployment
 
 BUCKET=potree-movie
 
+GITHUBPAGESFOLDER=~/hobu/helheim.lidar.io/movie/
+
 FILES=( dist/bundle.js dist/main.css index.html )
 LIBS=( libs build )
 
 npx webpack || exit 1
 
 for FILE in ${FILES[@]}; do
-    aws s3 cp ./$FILE s3://$BUCKET/$FILE || exit 1
+    aws s3 cp ./$FILE s3://$BUCKET/$FILE || exit 1 
+    cp ./$FILE $GITHUBPAGESFOLDER/$FILE
+    (cd $GITHUBPAGESFOLDER && git add $FILE)
 done
 
 for LIB in ${LIBS[@]}; do
+    cp -r ./$LIB $GITHUBPAGESFOLDER
     list=$(aws s3 ls s3://$BUCKET/$LIB/)
     if [ -z "$list" ]
     then
